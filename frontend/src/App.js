8cca88a771cc911ed19cac606dd92229d736672d
KwameSei <nathanielosei92@gmail.com>
2024-07-01 14:15:09 +0000

    Initial implementation of salary input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import { DataInput, DisplayResults } from './components';
-import { CalculateSalary } from './screens';
+import { CalculateSalary } from './screens'; 
 
 function App() {
+  const [results, setResults] = useState(null);
+
+  // Handler tp calculate the salary
+  const handleCalculate = (netSalary, allowances) => {
+    const salaryResults = CalculateSalary(netSalary, allowances);
+    setResults(salaryResults);
+  }
+
   return (
     <div className="App">
       <h1>Landing Page</h1>
+      <DataInput calculate={handleCalculate} />
     </div>
   );
 }
--- frontend/src/components/DataInput.js
+++ frontend/src/components/DataInput.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, TextField } from '@mui/material';
 
-const DataInput = () => {
+import classes from './salary.module.scss';
+
+const DataInput = ({ calculate }) => {
+  const [netSalary, setNetSalary] = useState('');
+  const [allowances, setAllowances] = useState('');
+
+  // Handler to submit the form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    calculate(parseFloat(netSalary), parseFloat(allowances));
+  }
+
+  // Listening for change in net salary input field
+  const salaryHandler = (e) => {
+    setNetSalary(e.target.value);
+  }
+
+  // Listening for change in allowances input field
+  const allowancesHandler = (e) => {
+    setAllowances(e.target.value);
+  }
+
   return (
-    <div>DataInput</div>
+    <div className={classes.input_main}>
+      <form onSubmit={handleSubmit} className={classes.input_content}>
+        <div className={classes.fields}>
+          <TextField
+            type='number'
+            value={netSalary}
+            onChange={salaryHandler}
+            required
+            />
+          <TextField
+            type='number'
+            value={allowances}
+            onChange={allowancesHandler}
+          />
+        </div>
+        <div className={classes.button}>
+          <Button type='submit'>Calculate</Button>
+        </div>
+      </form>
+    </div>
   )
 }
 
